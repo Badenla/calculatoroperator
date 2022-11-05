e98afe73de0455c5877d13806e82a1b74388496d
Badenla <baderinwadamola@gmail.com>
2022-11-05 13:22:26 +0100

    Add web

--- calc/views.py
+++ calc/views.py
@@ -4,23 +4,23 @@ from django.http import JsonResponse
 
 @api_view(['POST'])
 def operate(request):
-    error = None
+    invalid = None
     operations = ['addition', 'subtraction', 'multiplication']
     operation_act = request.data["operation_type"]
     x = request.data["x"]
     y = request.data["y"]
 
     if operation_act not in operations:
-        error = 'Invalid operation type'
+        invalid = 'Invalid operation type'
     if operation_act not in operations:
-        error = 'Invalid operation type. Options: <addition | subtraction | multiplication>'
+        invalid = 'Invalid operation type. Options: <addition | subtraction | multiplication>'
     if type(x) is not int:
-        error = 'x operand must be an integer'
+        invalid = 'x operand must be an integer'
     if type(y) is not int:
-        error = 'y operand must be an iteger'
+        invalid = 'y operand must be an iteger'
 
-    if error:
-        return JsonResponse({'msg': error}, status=400)
+    if invalid:
+        return JsonResponse({'msg': invalid}, status=400)
 
     if operation_act == 'addition':
         result = x + y
